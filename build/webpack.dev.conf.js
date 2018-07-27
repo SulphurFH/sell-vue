32b73c74ea16f0cfee06bcdd3e9e4d0af47b795a
fanghao9342@gmail.com <fanghao9342@gmail.com>
2018-07-27 17:14:21 +0800

    header component

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,9 +9,20 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+const express = require('express')
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
+
+const app = express()
+const appData = require('../data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
+const apiRoutes = express.Router()
+
+app.use('/api', apiRoutes)
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   module: {
@@ -22,6 +33,28 @@ const devWebpackConfig = merge(baseWebpackConfig, {
 
   // these devServer options should be customized in /config/index.js
   devServer: {
+    before (app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
+    },
     clientLogLevel: 'warning',
     historyApiFallback: {
       rewrites: [
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,30 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
+  linkActiveClass: 'active',
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/goods',
+      name: 'Goods',
+      component: (resolve) => require(['@/components/goods/goods'], resolve)
+    },
+    {
+      path: '/ratings',
+      name: 'Ratings',
+      component: (resolve) => require(['@/components/ratings/ratings'], resolve)
+    },
+    {
+      path: '/seller',
+      name: 'Seller',
+      component: (resolve) => require(['@/components/seller/seller'], resolve)
+    },
+    {
+      path: '*',
+      redirect: '/goods'
     }
   ]
-})
+});
